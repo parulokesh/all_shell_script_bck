8c6ec73e0403ef401d105edcb2e86f39b7413621
parulokesh <154958189+parulokesh@users.noreply.github.com>
2024-01-11 16:22:48 +0530

    Update equalornot.sh

--- equalornot.sh
+++ equalornot.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-echo "enter the value of first number"
-read num1
-echo "Enter the value of second number"
-read num2
-if [ $num1 -eq $num2 ]
+if [ $1 -eq $2 ]
 then
-	echo "$num1 is equal to $num2"
+echo "$1 is equal to $2"
 else
-	echo "$num1 is not equal to $num2"
+echo "$1 is not equal to $2"
 fi
